f68e0e17f70e5cf75b8c25b243a4c5f1594a95b6
danielhillsman <daniel.loves.junk@gmail.com>
2022-05-03 23:58:26 -0700

    added POST route for new tag

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -40,6 +40,13 @@ router.get('/:id', (req, res) => {
 // POST route to add a new tag
 router.post('/', (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+    
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 // PUT route to UPDATE tag name by ID
 router.put('/:id', (req, res) => {
