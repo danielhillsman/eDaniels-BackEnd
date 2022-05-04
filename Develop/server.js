d9ff4cf72140e083a44b4f7e1f22ab2b4ed56cfb
danielhillsman <daniel.loves.junk@gmail.com>
2022-05-03 21:58:04 -0700

    added comments to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,10 @@
+// Importing Files
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
-
+// Importing sequelizer
+const sequelize = require("./config/connection");
 const app = express();
+// Creating PORT 3001
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
@@ -11,6 +13,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
